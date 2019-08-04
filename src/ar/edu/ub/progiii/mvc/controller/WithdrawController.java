31849057114c58c9af1cac13a76c331c79d7a265
Fede <federicoa.franco@comunidad.ub.edu.ar>
2019-08-04 17:03:47 -0300

    Cambios para probar spring

--- src/ar/edu/ub/progiii/mvc/controller/WithdrawController.java
+++ src/ar/edu/ub/progiii/mvc/controller/WithdrawController.java
@@ -1,15 +1,25 @@
 package ar.edu.ub.progiii.mvc.controller;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class WithdrawController {
 	
 	@GetMapping("/")
-	public ModelAndView GetWDView() {
-		ModelAndView model = new ModelAndView("WithdrawView");
+	public ModelAndView GetView() {
+		ModelAndView model = new ModelAndView("index.html");
+		return model;
+	}
+	
+	@PostMapping("/")
+	public ModelAndView GetView2() {
+		ModelAndView model = new ModelAndView("index.html");
 		return model;
 	}
 
