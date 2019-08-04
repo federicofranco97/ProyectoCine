45f6c9df2b74592310e5c61fd28739b59c5c2f24
Fede <federicoa.franco@comunidad.ub.edu.ar>
2019-08-04 13:10:20 -0300

    Added fields for employee

--- src/ar/edu/ub/progiii/mvc/dto/EmployeeDTO.java
+++ src/ar/edu/ub/progiii/mvc/dto/EmployeeDTO.java
@@ -4,10 +4,12 @@ import java.sql.Date;
 
 public class EmployeeDTO extends PersonDTO{
 	private int EmployeeNumber;
-
-	public EmployeeDTO(String fullName, String address, String phoneNumber, String email, Date dateOfBirth,int EmployeeNumber) {
+	private String Rank;
+	
+	public EmployeeDTO(String fullName, String address, String phoneNumber, String email, Date dateOfBirth,int EmployeeNumber,String Rank) {
 		super(fullName, address, phoneNumber, email, dateOfBirth);
 		this.EmployeeNumber=EmployeeNumber;
+		this.Rank=Rank;
 	}
 
 	public int getEmployeeNumber() {
@@ -17,6 +19,15 @@ public class EmployeeDTO extends PersonDTO{
 	private void setEmployeeNumber(int employeeNumber) {
 		EmployeeNumber = employeeNumber;
 	}
+
+	public String getRank() {
+		return Rank;
+	}
+
+	private void setRank(String rank) {
+		Rank = rank;
+	}
 		
+	
 
 }
--- src/ar/edu/ub/progiii/mvc/model/Employee.java
+++ src/ar/edu/ub/progiii/mvc/model/Employee.java
@@ -4,10 +4,15 @@ import java.sql.Date;
 
 public class Employee extends Person{
 	private int EmployeeNumber;
+	private String HashedPassword;
+	private String Rank;
 
-	public Employee(String fullName, String address, String phoneNumber, String email, Date dateOfBirth,int EmployeeNumber) {
+	public Employee(String fullName, String address, String phoneNumber, String email, Date dateOfBirth,int EmployeeNumber,
+			String HashedPassword,String Rank) {
 		super(fullName, address, phoneNumber, email, dateOfBirth);
 		this.EmployeeNumber=EmployeeNumber;
+		this.HashedPassword=HashedPassword;
+		this.Rank=Rank;
 	}
 
 	public int getEmployeeNumber() {
@@ -17,6 +22,22 @@ public class Employee extends Person{
 	private void setEmployeeNumber(int employeeNumber) {
 		EmployeeNumber = employeeNumber;
 	}
-		
 
+	public String getHashedPassword() {
+		return HashedPassword;
+	}
+
+	private void setHashedPassword(String hashedPassword) {
+		HashedPassword = hashedPassword;
+	}
+
+	public String getRank() {
+		return Rank;
+	}
+
+	private void setRank(String rank) {
+		Rank = rank;
+	}
+	
+	
 }
