e2d02484508e4a7cfed0fc01fac590c50ddfcbe3
beelanselm <devicente.mbelen@gmail.com>
2019-08-18 18:04:59 -0300

    Correccion en menu controller

--- src/ar/edu/ub/progiii/mvc/controller/MenuController.java
+++ src/ar/edu/ub/progiii/mvc/controller/MenuController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@Controller("/menu")
 public class MenuController {
-    @GetMapping("/Admin")
+    @GetMapping("/menu")
     public ModelAndView GetMenu(){
         ModelAndView model = new ModelAndView("menu");
         return model;
