b4ae4fa8e7d525eedd6953965fb40b620c36e278
beelanselm <devicente.mbelen@gmail.com>
2019-08-18 16:32:43 -0300

    Controller de la vista menu agregado

--- src/ar/edu/ub/progiii/mvc/controller/MenuController.java
+++ src/ar/edu/ub/progiii/mvc/controller/MenuController.java
@@ -1,4 +1,14 @@
 package ar.edu.ub.progiii.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class MenuController {
+    @GetMapping("/Admin")
+    public ModelAndView GetMenu(){
+        ModelAndView model = new ModelAndView("menu");
+        return model;
+    }
 }
