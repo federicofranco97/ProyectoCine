e4935060e2255e5fa427187ad072147c3706896b
beelanselm <devicente.mbelen@gmail.com>
2019-08-22 20:12:50 -0300

    supresion de linea innecesaria en controller del menu y theme agregado en el html

--- src/ar/edu/ub/progiii/mvc/controller/MenuController.java
+++ src/ar/edu/ub/progiii/mvc/controller/MenuController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller("/menu")
+@Controller
 public class MenuController {
     @GetMapping("/menu")
     public ModelAndView GetMenu(){
