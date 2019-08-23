9a34338c00a42f2ae5d0eb1bbac80b1c20a98f16
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-08-23 01:55:09 -0300

    add comments js

--- resources/static/js/login.js
+++ resources/static/js/login.js
@@ -1,9 +1,21 @@
+// Se definio la variable asignandole el tag por ID del formulario
 var formEmployee = document.getElementById("formPass");
+// Se definieron variables de los campos pass y Id
 var pasword = document.getElementById("EmployeeId");
 var id = document.getElementById("EmployeePass");
+// Regex para campos vacios
 var whiteSpace = /^\s+$/;
-
+/**
+ * @descriptor Esta funcion va a ser ejecutada cuando se accione el boton submit del formulario
+ * 
+ * @param  {event}
+ */
 formEmployee.onsubmit = function(event){
+	/*
+	   Recorre los elementos del formulario y verifica por cada elemento del mismo
+	   que no este vacio ni con espacios en blanco, previniendo la accion por default del formulario
+	   y mostrando el aviso apropiado
+	*/
 	for (var i = 0; i < formEmployee.length; i++) 
 	{
 		if(formEmployee.elements[i].value == "" || whiteSpace.test(formEmployee.elements[i].value))
