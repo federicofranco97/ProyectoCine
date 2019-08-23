52ebc0d4905586531a05e1d8ea0800bc03dcf09a
beelanselm <devicente.mbelen@gmail.com>
2019-08-23 19:47:53 -0300

    correccion de estilo y modifcacion controller en vista menu

--- src/ar/edu/ub/progiii/mvc/controller/MenuController.java
+++ src/ar/edu/ub/progiii/mvc/controller/MenuController.java
@@ -1,15 +1,22 @@
 package ar.edu.ub.progiii.mvc.controller;
 
+import ar.edu.ub.progiii.mvc.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MenuController {
+
+    @Autowired
+    ClientService clientService;
+
     @GetMapping("/menu")
     public ModelAndView GetMenu(){
         ModelAndView model = new ModelAndView("menu");
-        model.addObject("modelDisplay",false);
+        int employeeCatNum = clientService.GetEmployeeCategory(5);
+        model.addObject("modelDisplay",employeeCatNum==1);
         return model;
     }
 }
