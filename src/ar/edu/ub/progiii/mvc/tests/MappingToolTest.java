068d905889976eb363825d467655e70141f0316e
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-08-27 02:38:56 -0300

    add testig from mapping tool

--- src/ar/edu/ub/progiii/mvc/tests/MappingToolTest.java
+++ src/ar/edu/ub/progiii/mvc/tests/MappingToolTest.java
@@ -2,12 +2,17 @@ package ar.edu.ub.progiii.mvc.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.ub.progiii.mvc.dto.BookingDTO;
 import ar.edu.ub.progiii.mvc.dto.ClientDTO;
 import ar.edu.ub.progiii.mvc.dto.EmployeeDTO;
+import ar.edu.ub.progiii.mvc.dto.FilmDTO;
 import ar.edu.ub.progiii.mvc.dto.TicketDTO;
 import ar.edu.ub.progiii.mvc.mapping.MappingTool;
 import ar.edu.ub.progiii.mvc.model.Employee;
@@ -17,7 +22,7 @@ import ar.edu.ub.progiii.mvc.service.ClientService;
 
 class MappingToolTest {
 
-	Connection connection;
+	java.sql.Connection connection;
 	Data dataManager;
 	ClientDTO clientDto;
 	EmployeeDTO employeeDTO;
@@ -33,6 +38,7 @@ class MappingToolTest {
 		dataManager = new Data();
 		mapping = new MappingTool();
 		clientService = new ClientService();
+		connection = ar.edu.ub.progiii.mvc.repository.Connection.getConnection();
 	}
 	
 	@Test
@@ -49,5 +55,42 @@ class MappingToolTest {
 		assertTrue(mapping.MapSQLBookingDTO(SPsql) instanceof BookingDTO);
 	}
 	
+	@Test
+	void MapDTOClientSQLtest() {
+		String SQLData = dataManager.GetClientByUId("2");
+		assertEquals(mapping.MapDTOClientSQL(SQLData).getFullName(),"Michael Myers");
+		assertTrue(mapping.MapDTOClientSQL(SQLData) instanceof ClientDTO);
+	}
+	
+	@Test
+	void MapDTOFilmSQLtest() throws SQLException {
+		String result = "";
+		if(connection != null) {
+            Statement stm = connection.createStatement();
+            String query="select * from pelicula where id=2";
+            ResultSet rst = stm.executeQuery(query);
+            while(rst.next()) {
+                result += (rst.getString("NombreCompleto"))+"_";
+                result += (rst.getString("Telefono"))+"_";
+                result += (rst.getString("Email"))+"_";
+                result += (rst.getString("Direccion"))+"_";
+                result += (rst.getString("FechaNac"))+"_";
+                result += (Integer.parseInt(rst.getString("id")))+"_";
+                result += (rst.getString("Clave"))+"_";
+                result += (rst.getString("CodRol"));
+            }
+       }
+		assertEquals(mapping.MapDTOFilmSQL(result).getCode(),2);
+		assertTrue(mapping.MapDTOFilmSQL(result) instanceof FilmDTO);
+	}
 
+	@Test
+	void MapDTOEmployeeSQLtest() {
+		String SQLData = dataManager.GetEmployeeByID("2");
+		assertEquals(mapping.MapDTOEmployeeSQL(SQLData).getEmployeeNumber(),2);
+		assertTrue(mapping.MapDTOEmployeeSQL(SQLData) instanceof EmployeeDTO);
+	}
+	
+	
+	
 }
