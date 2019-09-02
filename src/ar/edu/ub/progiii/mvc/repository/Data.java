3afc1ae713928893bff094c2020b351f8e63f264
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-09-02 01:35:12 -0300

    implements query builder on getActiveTickets data

--- src/ar/edu/ub/progiii/mvc/repository/Data.java
+++ src/ar/edu/ub/progiii/mvc/repository/Data.java
@@ -489,9 +489,9 @@ public class Data implements IData{
         //empiezo la conexion y recibo el resultado de la query
         try {
             if(connection != null) {
-                Statement stm = connection.createStatement();
-                String query="select * from tickets where estado='activo'";
-                ResultSet rst = stm.executeQuery(query);
+            	CQuerySelect querySelect = new CQuerySelect("tickets", "*");
+            	querySelect.addStatementCondition(Arrays.asList("estado='activo'"));
+            	ResultSet rst = querySelect.Run();
                 while(rst.next()) {
                     result += (rst.getString("ID").trim())+"_";
                     result += (rst.getString("Title").trim())+"_";
@@ -796,6 +796,6 @@ public class Data implements IData{
     }
     public static void main(String[] args) throws SQLException{
     	Data data = new Data();
-    	System.out.println(data.GetAllTickets());
+    	System.out.println(data.GetActiveTickets());
     }
 }
