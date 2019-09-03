0adb932d1983561ccd1b17a0ee430e5349789f41
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-09-03 15:02:59 -0300

    more QueryStoredProcedureWResponseTest is done

--- src/ar/edu/ub/progiii/mvc/repository/querys/QueryStoredProcedureWResponse.java
+++ src/ar/edu/ub/progiii/mvc/repository/querys/QueryStoredProcedureWResponse.java
@@ -59,7 +59,7 @@ public class QueryStoredProcedureWResponse implements IQueryBuilder<ResultSet> {
         for (int i = 0; i < Parameters.size(); i++) {
             result += Parameters.get(i);
             if(i!=Parameters.size()-1){
-                result += ", ";
+                result += ",";
             }
         }
         return result;
