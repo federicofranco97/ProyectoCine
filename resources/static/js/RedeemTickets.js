2f2f45ba2fdb3c464cf49167f552fb8388008ea5
beelanselm <devicente.mbelen@gmail.com>
2019-09-04 17:45:48 -0300

    Boton de busqueda y funcion agregada en vista redeem tickets

--- resources/static/js/RedeemTickets.js
+++ resources/static/js/RedeemTickets.js
@@ -1,4 +1,30 @@
-function BringBooking(){
-    var id = inputCode.value;
-    alert(id);
+function Validate(){
+    var idCode = document.getElementById("idCode").value;
+    console.log(idCode);
+    if(idCode === "" || idCode === "  "){
+        swal({
+                  title: "Error",
+                  text: "No ingreso un codigo valido",
+                  icon: "warning",
+                  buttons: true
+
+                })
+
+    }else{
+        swal({
+                  title: "Exito",
+                  text: "Codigo correcto",
+                  icon: "success",
+                  buttons: true
+
+                })
+    }
+
 }
+var idCode = document.getElementById("idCode").value;
+idCode.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("searchCode").click();
+  }
+});
