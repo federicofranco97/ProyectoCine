06aeb7cacd26878b4ea3d8d39b83b6b35069ba05
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-09-17 00:40:47 -0300

    add js Onsite

---
+++ resources/static/js/OnsiteSale.js
@@ -0,0 +1,37 @@
+var idMovie = -1;
+var idFunction;
+
+/*   
+    Borra todos los colores de fonde de los tag td
+*/
+$('td').on('click', function(){
+   $('td').css("background-color" , "");
+});
+
+/* 
+   Llena de un determinado color el fondo de los tag td y guarda su valor en una variable
+*/
+$('td').on('click', function(){
+   $(this).css("background-color" , "rgb(4, 124, 179)");
+});
+
+/* 
+   Guarda el valor del id de la pelicula en una variable
+*/
+function saveValue(element){
+	var valueId = $(element).attr("value");
+	idMovie = valueId;
+}
+
+/* 
+   Guarda el valor del id de la funcion en una variable y redirecciona a la ruta del controller correspondiente
+*/
+function saveBooking(element){
+	if (idMovie == -1){
+		swal("Aviso", "Seleccione una pelicula!", "warning");
+	}
+	else{
+		idFunction = $(element).attr("value");
+		window.open("/presencial_cantidadEntradas?ids="+idFunction+"_"+idMovie,"_self");
+	}
+}
