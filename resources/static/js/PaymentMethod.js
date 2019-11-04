7815adf550e28d46069f5f2e098b998af56357ad
beelanselm <devicente.mbelen@gmail.com>
2019-11-03 23:51:10 -0300

    pantalla metodo pago agregada

---
+++ resources/static/js/PaymentMethod.js
@@ -0,0 +1,47 @@
+function SendMoney(){
+    var amount1 = document.getElementById("inCash").value;
+    var amount2 = document.getElementById("amountTotal").innerText;
+    var payMethod = document.getElementById("payMethod").value;
+    if(amount1 === amount2)
+    {
+        swal({
+                          title: "Exito",
+                          text: "Venta efectuada con éxito!",
+                          icon: "success",
+                          buttons: true
+                        });
+        window.open("/metodo_pago?payParam="+payMethod,"_self");
+        setTimeout("returnToMenu()",1500);
+
+    }
+
+}
+
+function returnToMenu(){
+    window.open("/menu","_self");
+}
+
+function autoSub(){
+    var amount1= document.getElementById("inCash").value;
+    var amount2= document.getElementById("amountTotal").innerText;
+    var change= document.getElementById("change");
+    var changeResult= (amount1-amount2);
+    change.disabled="false";
+    change.value= changeResult;
+}
+
+function cashValue(){
+    var moneyVal = document.getElementById("payMethod");
+    var cash = document.getElementById("cashPay");
+    if(cash.onclick){
+        moneyVal.value = 1;
+     }
+ }
+
+function creditValue(){
+    var moneyVal = document.getElementById("payMethod");
+    var credit = document.getElementById("ml");
+    if(credit.onclick){
+        moneyVal.value = 2;
+     }
+}
--- src/ar/edu/ub/progiii/mvc/controller/PaymentMethodController.java
+++ src/ar/edu/ub/progiii/mvc/controller/PaymentMethodController.java
@@ -1,12 +1,30 @@
 package ar.edu.ub.progiii.mvc.controller;
 
+import ar.edu.ub.progiii.mvc.repository.Data;
 import ar.edu.ub.progiii.mvc.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Controller
 public class PaymentMethodController {
     @Autowired
     ClientService clientService;
+    Data data = new Data();
+    @GetMapping("/metodo_pago")
+    public ModelAndView GetPaymentMethod(@RequestParam("payParam") String payValue, HttpServletRequest request){
+        int employeeId = (int)request.getSession().getAttribute("EmployeeId");
+        int payNumber = Integer.parseInt(payValue);
+        ModelAndView model = new ModelAndView("PaymentMethod");
+        data.UpdateLastBooking("CodEstadoReserva",3, data.GetLastBookingByEmployeeId(employeeId));
+        data.UpdateLastBooking("CodMedioPago",payNumber, data.GetLastBookingByEmployeeId(employeeId));
+        model.addObject("Content", Arrays.asList("Aviso", "Esta siendo redirigido al menu", "1"));
+         return model;
 
+    }
 }
