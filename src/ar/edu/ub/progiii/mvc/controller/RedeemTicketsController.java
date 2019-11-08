1a2dbf63a75db6e72e1188755e13f59b1f9682f2
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-11-08 16:10:17 -0300

    bla

--- src/ar/edu/ub/progiii/mvc/controller/RedeemTicketsController.java
+++ src/ar/edu/ub/progiii/mvc/controller/RedeemTicketsController.java
@@ -29,33 +29,36 @@ public class RedeemTicketsController {
         ModelAndView model = new ModelAndView("RedeemTickets");
         return model;
     }
+    
     /**
     Metodo que te lleva a la vista para byscar reservas
+    @param BookingNumber
      */
     @GetMapping("/buscar_reserva")
     public ModelAndView GetBuscarReserva(@RequestParam("bookingId")String BookingNumber){
-
-        if(clientService.GetBookingById(BookingNumber) == null){
-            ModelAndView modelError = new ModelAndView("RedeemTickets");
-            modelError.addObject("Content", Arrays.asList("Error","No se ha encontrado la reserva","1"));
-            return modelError;
+    	ModelAndView model = new ModelAndView("RedeemTickets");
+        if(clientService.GetBookingById(BookingNumber) == null || clientService.GetBookingById(BookingNumber).getChannelCode() != 2){
+            model.addObject("Content", Arrays.asList("Error","No se ha encontrado la reserva","1"));
+            return model;
         }
-        ModelAndView modelOk = new ModelAndView("RedeemTickets");
         OnlineBookingDTO booking = new OnlineBookingDTO(clientService.GetBookingById(BookingNumber));
         FilmDTO film = clientService.GetFilmById(Integer.parseInt(booking.getMovieName()));
-        modelOk.addObject("Content", null);
-        modelOk.addObject("Booking",booking);
-        modelOk.addObject("pelicula",film);
-        modelOk.addObject("funcion",clientService.GetCinemaShow(booking.getShow()).getStartTime());
-        modelOk.addObject("msj","yes");
-        return modelOk;
-
+        model.addObject("Content", null);
+        model.addObject("Booking",booking);
+        model.addObject("pelicula",film);
+        model.addObject("funcion",clientService.GetCinemaShow(booking.getShow()).getStartTime());
+        model.addObject("msj","yes");
+        return model;
     }
+    
+    /**
+    Metodo para imprimir reserva
+    @param BookingNumber
+     */
     @GetMapping("/imprimir_reserva")
     public ModelAndView getImprimirReserva(@RequestParam("bookingId")String BookingNumber){
         clientService.RedeemBooking(BookingNumber);
         ModelAndView model = new ModelAndView("PrintBooking");
-
         return model;
     }
 
